f0ca54c2c36d3b063e543098d02b21bac9c35540
dagreatcode <dagreatcode@gmail.com>
2020-12-17 10:53:04 -0500

    working on gitignore

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
     console.log("The document is ready!");
+
+    // const settings = {
+    //     "async": true,
+    //     "crossDomain": true,
+    //     "url": "https://car-code.p.rapidapi.com/obd2/P0001",
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-key": "d45bb63eb5mshebc4e0e524334b5p10227ejsn3cb49f17bfa1",
+    //         "x-rapidapi-host": "car-code.p.rapidapi.com"
+    //     }
+    // };
+    
+    // $.ajax(settings).done(function (response) {
+    //     console.log(response);
+    // });
 })
