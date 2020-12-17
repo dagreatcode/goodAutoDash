bc6891fce1f3b23790ceb8d8a0350e6299156c54
dagreatcode <dagreatcode@gmail.com>
2020-12-17 16:08:51 -0500

    working on the html, guess i was not done

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,34 +1,46 @@
 $(document).ready(function () {
+  console.log("The document is ready!");
+
+  var searchTerm;
+
+  function apiCall(){
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://car-code.p.rapidapi.com/obd2/P0001",
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "d45bb63eb5mshebc4e0e524334b5p10227ejsn3cb49f17bfa1",
+        "x-rapidapi-host": "car-code.p.rapidapi.com",
+      },
+    };
+    $.ajax(settings).then(function (response) {
+      // console.log(response);
+      // for(var i = 0; i < response.length; i++){
+      //     console.log(response[i]);
+      // };
+      console.log(response.code);
+      console.log(response.cause);
+      console.log(response.definition);
+  
+      $("#code-display").text(response.code);
+      $("#cause-display").text(response.cause);
+      $("#definition-display").text(response.definition);
+    });
+  }
 
   $("#search-form").on("submit", function(event){
     event.preventDefault();
-    searchTerm = $("")
+    searchTerm = $("#specificSizeInputName").val();
+    if (searchTerm){
+      apiCall();
+    }else{
+      alert("I am still Working on this... Vincent.")
+    }
   });
 
-  console.log("The document is ready!");
-  const settings = {
-    async: true,
-    crossDomain: true,
-    url: "https://car-code.p.rapidapi.com/obd2/P0001",
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "d45bb63eb5mshebc4e0e524334b5p10227ejsn3cb49f17bfa1",
-      "x-rapidapi-host": "car-code.p.rapidapi.com",
-    },
-  };
-  $.ajax(settings).then(function (response) {
-    // console.log(response);
-    // for(var i = 0; i < response.length; i++){
-    //     console.log(response[i]);
-    // };
-    console.log(response.code);
-    console.log(response.cause);
-    console.log(response.definition);
 
-    $("#code-display").append(response.code);
-    $("#cause-display").append(response.cause);
-    $("#definition-display").append(response.definition);
-  });
+
 //   function displayResultsToPage(array){
 //     $("#display").append(array);
 //   }
