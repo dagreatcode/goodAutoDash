bc86cb58ed531d0f214fc4a6d53f97cf3a31ed59
dagreatcode <dagreatcode@gmail.com>
2020-12-19 00:11:24 -0500

    new page and another api

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -2,22 +2,22 @@ $(document).ready(function () {
   console.log("The document is ready!");
 
   var searchTerm;
-  var searchTerm2;
-  
+  var submitTerm;
+  var lookupTerm;
+
   var codeD = $("#code-display");
-  var causeD =  $("#cause-display");
+  var causeD = $("#cause-display");
   var definitionD = $("#definition-display");
-  var searchBtn =  $("#search-button");
-  var inputC =  $("#input-control");
-  var submitBtn =  $("#submit-button");
-  var inputC2 =  $("#input-control2");
+  var searchBtn = $("#search-button");
+  var inputC = $("#input-control");
+  var submitBtn = $("#submit-button");
+  var inputC2 = $("#input-control2");
   var brandD = $("#brand-display");
   var dataD = $("#data-display");
   var recallD = $("#recall-display");
   // var odbCode = "P0001";
 
-
-  function apiCall(){
+  function apiCall() {
     console.log("api Call");
     const settings = {
       async: true,
@@ -42,29 +42,26 @@ $(document).ready(function () {
       causeD.text(response.cause);
       definitionD.text(response.definition);
       searchBtn.attr("disabled", false);
-     
-      
-  
+
       // $("#code-display").text(response.code);
       // $("#cause-display").text(response.cause);
       // $("#definition-display").text(response.definition);
       // $("#search-button").attr("disabled", false);
-
     });
   }
 
-  function apiVin(){
+  function apiVin() {
     const settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://cis-vin-decoder.p.rapidapi.com/vinDecode?vin=" +vinPin,
-      "method": "GET",
-      "headers": {
+      async: true,
+      crossDomain: true,
+      url: "https://cis-vin-decoder.p.rapidapi.com/vinDecode?vin=" + vinPin,
+      method: "GET",
+      headers: {
         "x-rapidapi-key": "d45bb63eb5mshebc4e0e524334b5p10227ejsn3cb49f17bfa1",
-        "x-rapidapi-host": "cis-vin-decoder.p.rapidapi.com"
-      }
+        "x-rapidapi-host": "cis-vin-decoder.p.rapidapi.com",
+      },
     };
-    
+
     $.ajax(settings).done(function (response) {
       console.log(response);
       // // console.log(response.brandName);
@@ -77,38 +74,62 @@ $(document).ready(function () {
     });
   }
 
+  function apiAutoInfo() {
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url:
+        "https://cis-automotive.p.rapidapi.com/getInactiveModels?brandName=Ford",
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "d45bb63eb5mshebc4e0e524334b5p10227ejsn3cb49f17bfa1",
+        "x-rapidapi-host": "cis-automotive.p.rapidapi.com",
+      },
+    };
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+  }
 
-
-
-  $("#search-form").on("submit", function(event){
+  $("#search-form").on("submit", function (event) {
     event.preventDefault();
-    submitBtn.attr("disabled", true);
+    searchBtn.attr("disabled", true);
     searchTerm = inputC.val();
     odbCode = inputC.val();
-    if (searchTerm){
+    if (searchTerm) {
       apiCall();
-    }else{
-      alert("I am still Working on this... Vincent.")
+    } else {
+      alert("I am still Working on this... Vincent.");
     }
   });
 
-
-  $("#search-form2").on("submit", function(event){
+  $("#submit-form").on("submit", function (event) {
     event.preventDefault();
-    searchBtn.attr("disabled", true);
-    searchTerm2 = inputC2.val();
+    submitBtn.attr("disabled", true);
+    submitTerm = inputC2.val();
     vinPin = inputC2.val();
-    if (searchTerm2){
+    if (submitTerm) {
       apiVin();
+    } else {
+      alert("You Must Type something.");
+    }
+  });
+
+  $("#lookup-form3").on("submit", function(event){
+    event.preventDefault();
+    lookupBtn.attr("disabled", true);
+    lookupTerm = inputC2.val();
+    vinPin = inputC3.val();
+    if (lookupTerm){
+      apiAutoInfo();
     }else{
       alert("You Must Type something.")
     }
   });
 
-
-//   function displayResultsToPage(array){
-//     $("#display").append(array);
-//   }
+  //   function displayResultsToPage(array){
+  //     $("#display").append(array);
+  //   }
 });
 
 //////////////////////////////////////////////////////////////////
